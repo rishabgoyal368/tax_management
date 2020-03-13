d2fff6f0013e19de345f14338d4aa5d98331a9c7
shikha <shikha@softuvo.com>
2020-03-13 17:08:24 +0530

    header

--- resources/views/Layout/header.blade.php
+++ resources/views/Layout/header.blade.php
@@ -203,7 +203,7 @@
                                         <span class="media align-items-center">
                                             <span class="lnr lnr-power-switch mr-3"></span>
                                             <span class="media-body text-truncate text-left">
-                                                <span class="text-truncate text-left">Logout</span>
+                                                <span class="text-truncate text-left">Logoutabcdef</span>
                                             </span>
                                         </span>
                                     </a>
